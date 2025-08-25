7d90e29cca1517afa8e874655664af36c319f344
Shanilka-Yapa <shanilka.thulshani@gmail.com>
2025-08-25 11:03:43 +0530

    Initial Vite React setup

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5435,   // 👈 change this to your port
+  },
+})
